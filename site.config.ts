2f5253250341a092a170510201ebee338b8ee572
zyglz <25851616@qq.com>
2026-07-26 21:37:53 +0800

    第一次提交

--- site.config.ts
+++ site.config.ts
@@ -1,10 +1,10 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://your.site',
+  url: 'https://blog.ttdd.top',
   lang: 'zh-CN',
-  title: 'AIOVTUE',
-  subtitle: '雪',
+  title: 'Tidus',
+  subtitle: 'Td',
   author: {
     name: 'AIOVTUE',
     avatar: 'https://r2tc.20030327.xyz/file/博客/主题/1780655293662_avatar_me.jpg.PNG',   //站点头像
@@ -13,7 +13,7 @@ export default defineSiteConfig({
       message: '发呆ing...',
       },
   },
-  description: '雨是神的烟花',
+  description: 'Tidus的博客',
 
   favicon: '/favicon.png',
 
@@ -26,25 +26,25 @@ export default defineSiteConfig({
     },
     {
       name: 'GitHub',
-      link: 'https://github.com/AIOVTUE',
+      link: 'https://github.com/Tidus',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
     {
       name: '微信公众号',
-      link: 'https://r2tc.20030327.xyz/file/博客/主题/1780654223927_mmexport1780654189207.jpeg',
+      link: 'https://image.zyglz.com/file/zyglz/2026/1783501660682.jpg',
       icon: 'i-ri-wechat-2-line',
       color: '#1AAD19',
     },
     {
       name: 'E-Mail',
-      link: 'mailto:x@yybb.us',
+      link: 'mailto:admin@ttdd.top',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
     },
     {
-      name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
+      name: '资源管理站',
+      link: 'https://www.zyglz.com',
       icon: 'i-ri-train-line',
       color: '#8cb1b3',
     },
@@ -85,13 +85,13 @@ export default defineSiteConfig({
     methods: [
       {
         name: '支付宝',
-        url: 'https://i.postimg.cc/fyYTncnJ/IMG-20240915-155521.jpg',
+        url: 'https://image.zyglz.com/file/zyglz/2026/1783501660682.jpg',
         color: '#00A3EE',
         icon: 'i-ri-alipay-line',
       },
       {
         name: '微信支付',
-        url: 'https://i.postimg.cc/26s8KHts/IMG-20240915-155547.png',
+        url: 'https://image.zyglz.com/file/zyglz/2026/1783501660682.jpg',
         color: '#2DC100',
         icon: 'i-ri-wechat-pay-line',
       },
